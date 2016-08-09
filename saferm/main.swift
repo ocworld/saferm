5084e174cb07501d9cf3026dc8e23ec3bc815738
OhKeunhyun <ocworld@gmail.com>
2016-08-09 22:30:08 +0900

    Initial Commit

---
+++ saferm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  saferm
+//
+//  Created by OhKeunhyun on 2016. 8. 9..
+//  Copyright © 2016년 Keunhyun Oh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
