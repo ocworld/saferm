c61632e896e04e89fa7c7473a9212a05ce738bb3
OhKeunhyun <ocworld@gmail.com>
2016-08-13 00:04:25 +0900

    redesign. 전체 구조를 dispatchqueue를 사용하도록 변경. 휴지통으로 파일 이동됨

---
+++ saferm/func.swift
@@ -0,0 +1,152 @@
+//
+//  func.swift
+//  saferm
+//
+//  Created by OhKeunhyun on 2016. 8. 12..
+//  Copyright © 2016년 Keunhyun Oh. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+func printUsage() {
+    print("usage: saferm [-f | -i] [-dPRrvW] file ...")
+    print("unlink file")
+}
+
+func parseArgs() -> (opts: Set<Character>, dirs: Set<URL>, files: Set<URL>)?
+{
+    let supportedOptions : Set<Character> = ["f","i","d","P","R","r","v","W"]
+    
+    let argc = Process.arguments.count
+    if argc <= 1 {
+        assert(argc > 1)
+        return nil
+    }
+    
+    let args = Process.arguments.dropFirst()
+    
+    var opts = Set<Character>()
+    var dirs = Set<URL>()
+    var files = Set<URL>()
+    for arg in args {
+        guard let first = arg.characters.first else {
+            assert(arg.characters.first != nil)
+            return nil
+        }
+        
+        //options
+        if first == "-" {
+            let dataOpts = arg.characters.dropFirst()
+            for opt in dataOpts {
+                if supportedOptions.contains(opt) {
+                    opts.insert(opt)
+                }
+            }
+        }
+        else
+        {
+            let tmpPath = NSString(string: arg)
+            let standardedPath = tmpPath.standardizingPath
+            var isDir : ObjCBool = false
+            if !FileManager.default.fileExists(atPath: standardedPath, isDirectory: &isDir) {
+                continue
+            }
+            
+            let url = URL(fileURLWithPath: arg)
+            if isDir.boolValue {
+                dirs.insert(url)
+            } else {
+                files.insert(url)
+            }
+        }
+    }
+    
+    return (opts, dirs, files)
+}
+
+func selectedUrls(opts: Set<Character>, dirs: Set<URL>, files: Set<URL>) -> [URL]? {
+    var urls:Set<URL> = Set<URL>(files)
+    
+    if opts.contains("r") {
+        urls = urls.union(dirs)
+    }
+    else {
+        guard dirs.isEmpty else {
+            assert(dirs.isEmpty)
+            return nil
+        }
+    }
+    
+    return urls.sorted() {$0.path < $1.path}
+}
+
+func moveToTrash(urls: [URL], withForceOption forced: Bool) {
+    if forced {
+        NSWorkspace.shared().recycle(urls) {
+            (_, error) in
+
+            if let err = error {
+                print(err)
+            }
+            
+            exit(0)
+        }
+    }
+    else
+    {
+        if !urls.isEmpty {
+            guard let url = urls.first else {
+                exit(0)
+            }
+            
+            let newUrls = Array(urls.dropFirst())
+            if confirm(url) {
+                NSWorkspace.shared().recycle([url]) {
+                    (_, error) in
+
+                    if let err = error {
+                        print(err)
+                    }
+                    
+                    if newUrls.isEmpty {
+                        exit(0)
+                    } else {
+                        moveToTrash(urls: newUrls, withForceOption: forced)
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    moveToTrash(urls: newUrls, withForceOption: forced)
+                }
+            }
+        }
+    }
+}
+
+func confirm(_ url: URL) -> Bool {
+    var confirmed = false
+    keyboardinputloop: while(true)
+    {
+        print("\(url.path) (y/n): ", terminator: "")
+        
+        if let input = readLine() {
+            switch(input)
+            {
+            case "y", "Y", "yes":
+                confirmed = true
+                break keyboardinputloop
+                
+            case "n", "N", "no":
+                confirmed = false
+                break keyboardinputloop
+                
+            default:
+                print("Please input yes or no")
+                continue keyboardinputloop
+            }
+        }
+    }
+    
+    return confirmed
+}
--- saferm/main.swift
+++ saferm/main.swift
@@ -8,105 +8,23 @@
 
 import Foundation
 import Darwin
-import AppKit
 
-func printUsage() {
-    print("usage: saferm [-f | -i] [-dPRrvW] file ...")
-    print("unlink file")
-}
-
-let supportedOptions : Array<Character> = ["f","i","d","P","R","r","v","W"]
-
-let argc = Process.arguments.count
-if argc <= 1 {
-    printUsage()
-    exit(1)
-}
-
-let args = Process.arguments.dropFirst()
-
-var opts = Set<Character>()
-var dirs = Set<URL>()
-var files = Set<URL>()
-for arg in args {
-    guard let first = arg.characters.first else {
-        break
+func main() {
+    guard let info = parseArgs() else {
+        assert(parseArgs() != nil)
+        printUsage()
+        exit(1)
     }
     
-    //options
-    if first == "-" {
-        let dataOpts = arg.characters.dropFirst()
-        for opt in dataOpts {
-            if supportedOptions.contains(opt) {
-                opts.insert(opt)
-            }
-        }
+    guard let urls = selectedUrls(opts: info.opts, dirs: info.dirs, files: info.files) else {
+        assert(selectedUrls(opts: info.opts, dirs: info.dirs, files: info.files) != nil)
+        printUsage()
+        exit(1)
     }
-    else
-    {
-        let tmpPath = NSString(string: arg)
-        let standardedPath = tmpPath.standardizingPath
-        var isDir : ObjCBool = false
-        guard FileManager.default.fileExists(atPath: standardedPath, isDirectory: &isDir) else {
-            break
-        }
-        
-        let url = URL(fileURLWithPath: arg)
-        if isDir.boolValue {
-            dirs.insert(url)
-        } else {
-            files.insert(url)
-        }
-    }
+    
+    let forced = info.opts.contains("f")
+    moveToTrash(urls: urls, withForceOption: forced)
 }
 
-var paths:Set<URL> = Set<URL>()
-if opts.contains("r") {
-    paths = paths.union(dirs)
-}
-
-paths = paths.union(files)
-let sortedPaths = paths.sorted() {$0.path > $1.path}
-let isForced = opts.contains("f")
-
-if isForced {
-    NSWorkspace.shared().recycle(sortedPaths)
-    { (urlInfo, error) in
-        print(urlInfo)
-        if let err = error {
-            print(err)
-        }
-    }
-}
-else {
-    for path in sortedPaths {
-        var isYes = false
-        
-        keyboardinputloop: while(true)
-        {
-            print("\(path.path) (y/n): ", terminator: "")
-            
-            if let input = readLine() {
-                switch(input)
-                {
-                case "y", "Y", "yes":
-                    isYes = true
-                    break keyboardinputloop
-                    
-                case "n", "N", "no":
-                    isYes = false
-                    break keyboardinputloop
-                    
-                default:
-                    print("Please input yes or no")
-                    continue keyboardinputloop
-                }
-            }
-        }
-        
-        if isYes {
-            NSWorkspace.shared().recycle([path], completionHandler: { (urlInfo, error) in if let err = error { print(err) } })
-        }
-    }
-}
-
+DispatchQueue.main.async(execute: main)
+RunLoop.main.run()
